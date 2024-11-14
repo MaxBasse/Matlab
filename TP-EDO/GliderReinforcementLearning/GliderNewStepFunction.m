aa3d19cece19b3883b0cda084a07ddc16dbff36d
YannPIR <yannluc.pierre@gmail.com>
2024-11-14 20:21:47 +0100

    nouvelle version
    variable en moins, et nouveau code step. Agent 3 est semi-entrainé, et agent4 c'est le brouillon que tu peux entrainer en boucle

--- TP-EDO/GliderReinforcementLearning/GliderNewStepFunction.m
+++ TP-EDO/GliderReinforcementLearning/GliderNewStepFunction.m
@@ -1,4 +1,4 @@
-function [NextObs, Reward, IsDone, NextState] = GliderStepFunction(Action, State)
+function [NextObs, Reward, IsDone, NextState] = GliderNewStepFunction(Action, State)
 %Custom step function for the glider env, computes the pos for one step
 
 % Consts
@@ -17,37 +17,26 @@ yTreshold = 0;
 yMaximum =20;
 
 % x minimum for the glider i.e it must crash after x = 400 
-xMinimum = 300;
+xMinimum = 210;
 
 % theta maximum for the glider i.e how straight we want it to glide
-thetaMaximum =pi/6;
+thetaMaximum =1.2;
 
 %-----------
 
 %Variable to compute the reward
-Y = State(2);
-X=State(1);
-Theta = State(4);
+distY = yMaximum-State(2);
+distTheta = abs(State(4)-thetaMaximum);
+w = sign(abs(2-Action));
+a = 5; k = 30;
+RewardForNotLanding = ...
+State(1)*distY*abs(1-State(4)) - k*max(0,(Action-k)*w);
 
-if X<250 && Y<yMaximum
-    RewardForLanding = X^2; %Rewarded for heading down at the beginning
-elseif Y<yMaximum   %If low altitude
-    if abs(Theta)<thetaMaximum %Rewarded only if not oscillating
-        RewardForLanding=X*1e4/(abs(Y)+0.01)^2;
-    else
-        RewardForLanding = -1e5/X^2;
-    end
-else
-    RewardForLanding=-X^2;
-end
-% If the glider crashed, it's rewarded only after a certain x
-if State(1)<xMinimum
-    PenaltyForCrashing = -1e6*X;
-else
-    PenaltyForCrashing = X^2;
-end
+<<<<<<< Updated upstream:TP-EDO/GliderReinforcementLearning/GliderStepFunction.asv
 
-
+=======
+PenaltyForCrashing = -1e5;
+>>>>>>> Stashed changes:TP-EDO/GliderReinforcementLearning/GliderNewStepFunction.m
 % Perform RK4 to calculate next state.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 NextState = RK4_2(g,h,Action,State);
@@ -56,12 +45,17 @@ NextState = RK4_2(g,h,Action,State);
 NextObs = NextState;
 
 % Check terminal condition.
+<<<<<<< Updated upstream:TP-EDO/GliderReinforcementLearning/GliderStepFunction.asv
 
 IsDone = Y < yTreshold;
+=======
+IsDone = NextState(2) < yTreshold;
+>>>>>>> Stashed changes:TP-EDO/GliderReinforcementLearning/GliderNewStepFunction.m
 
 
 % Calculate reward.
 if ~IsDone
+<<<<<<< Updated upstream:TP-EDO/GliderReinforcementLearning/GliderStepFunction.asv
     Reward = RewardForApproch
 else
     if Landed
@@ -69,6 +63,11 @@ else
     else
         Reward = PenaltyForCrashing
     end
+=======
+    Reward = RewardForNotLanding;
+else
+    Reward= PenaltyForCrashing;
+>>>>>>> Stashed changes:TP-EDO/GliderReinforcementLearning/GliderNewStepFunction.m
 end
 
 end
@@ -85,7 +84,6 @@ function NextState = RK4_2(g,h,Action,State)
     k4=rhs2(g,Action,Y4);
     Y=State+h*(k1+2*k2+2*k3+k4)/6;
     NextState=Y;
-    NextState(5)=Action;
 end
 %----------------------------------
 function dy = rhs2(g,Action,State)
@@ -104,7 +102,7 @@ function dy = rhs2(g,Action,State)
         0.0202
         0.0229
         0.0265];
-    dy = zeros(5,1);
+    dy = zeros(4,1);
     
     dy(1) = State(3)*cos(State(4));
     dy(2) = State(3)*sin(State(4));
